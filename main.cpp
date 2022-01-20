ad8bae34edc5fe1e0e7badac9b0dd664effd242c
Georgi Mihaylov <georgimihaylov2002@gmail.com>
2022-01-20 19:10:21 +0200

    Added main file

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/**
+*
+* Solution to course project # 13
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2021/2022
+*
+* @author Georgi Mihaylov
+* @idnumber 3MI0600047
+* @compiler VC
+*
+*/
+
+
+#include "play.h";
+
+int main()
+{
+	playMinesweeper();
+
+	return 0;
+}
