025f2e8d2b9637404fa6a67981fc1c05795cc75f
Georgi Mihaylov <georgimihaylov2002@gmail.com>
2022-01-20 19:54:40 +0200

    constants file

---
+++ constants.h
@@ -0,0 +1,8 @@
+#pragma once
+
+const short SIDE = 9;
+const short MINES = 10;
+const short MAX_CHARACTERS_IN_A_COMMAND = 6;
+const char CELL_CHARACTER = '-';
+const char MINE_CHARACTER = '*';
+const char FLAG_CHARACTER = 'F';
